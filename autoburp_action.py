a15d1aa54f6c7e58b4e920dae7abacd491d40679
brianlam38 <brian.lam38@gmail.com>
2018-01-22 17:33:48 +1100

    adding stackstorm autoburp version

--- autoburp_action.py
+++ autoburp_action.py
@@ -6,18 +6,77 @@ import sys
 import tempfile
 import time
 import re
-
 import requests
 
-target_url = None           # user-specified target
-api_port = "8090"           # default api port
-proxy_url = "http://127.0.0.1"     # default proxy url
+# stackstorm action runner module
+from st2actions.runners.pythonrunner import Action
+
+# grab Burp container IP
+network_file = open("/opt/stackstorm/common/networks.txt", "r")
+data = network_file.readlines()
+BURP_IP = ''
+for line in data:
+    if re.search('BURP_IP', line):
+        BURP_IP = re.sub('BURP_IP=', '', line)
+BURP_IP = BURP_IP.replace('\n', '')
+network_file.close()
+
+api_port = "8090"                   # default api port
+proxy_url = "http://" + BURP_IP     # default Burp proxy URL (localhost of Burp container)
 proxy_port = "8080"         # default proxy port
 
-"""
-Add target_url to scope
-"""
-def add_scope():
+class Burp_Scan(Action):
+    def run(self, url):
+        print("Parsed URL = {}".format(url))
+        target_url = url
+
+        # adding target to scope
+        print("[+] Adding target url to scope ...")
+        add_scope(target_url)
+
+        """
+        Need better way to know when spider queue = 0.
+        Currently, just waiting a static amount of time.
+        """
+        print("[+] Spidering target url ...")
+        spider_target(target_url)
+
+        waiting = 0
+        while waiting != 100:
+            if waiting % 5 == 0:
+                print("Still Spidering")
+            time.sleep(1)
+            waiting += 1
+
+        print("[+] Spidering complete ...")
+
+        # check if site map content exists
+        print("[+] Checking spidered URLs ...")
+        content = site_map(target_url)
+        if not content:
+            print("No URL's have been discovered, scan cannot proceed.")
+            sys.exit(1)
+        else:
+            print("URL's discovered:")
+            for url in content['messages']:
+                print(url['url'])
+
+        print("[+] Performing active scan ...")
+        active_scan(target_url)
+
+        # get scan status
+        while scan_status() != 100:
+            time.sleep(20)
+        print("\n[+] Scan completed")
+
+        # generate scan report
+        scan_report(target_url)
+
+        # stop API service
+        #burp_stop()
+
+# Add target_url to scop
+def add_scope(target_url):
     try:
         r = requests.put(
             "{}:{}/burp/target/scope?url={}".format(
@@ -32,10 +91,8 @@ def add_scope():
         print("Error adding the target scope: {}".format(e))
         sys.exit(1)
 
-"""
-Spider the target_url
-"""
-def spider_target():
+# spider the target_url
+def spider_target(target_url):
     try:
         r = requests.post(
             "{}:{}/burp/spider?baseUrl={}".format(
@@ -50,10 +107,8 @@ def spider_target():
         print("Error spidering target scope: {}".format(e))
         sys.exit(1)
 
-"""
-Retrieve site map content
-"""
-def site_map():
+# Retrieve site map content
+def site_map(target_url):
     print("[+] Retrieving the Burp site map ...")
     try:
         r = requests.get(
@@ -67,7 +122,6 @@ def site_map():
     except requests.exceptions.RequestException as e:
         print("Error retrieving Burp site map items: {}".format(e))
         sys.exit(1)
-    # continue if no exception
     else:
         resp = r.json()
         print("SITE MAP:\n {}".format(resp))
@@ -85,10 +139,8 @@ def site_map():
         else:
             print("[-] Burp site map is empty")
 
-"""
-Perform active scan
-"""
-def active_scan():
+# Perform active scan
+def active_scan(target_url):
     """Send a URL to Burp to perform active scan"""
     try:
         r = requests.post(
@@ -104,9 +156,7 @@ def active_scan():
         print("Error adding {} to the scan queue: {}".format(target_url, e))
         sys.exit(1)
 
-"""
-Check scan status
-"""
+# Check scan status
 def scan_status():
     """Get the percentage completed for the scan queue items"""
     try:
@@ -127,10 +177,8 @@ def scan_status():
         sys.stdout.flush()
         return resp['scanPercentage']
 
-"""
-Download scan report for the target URL in XML format
-"""
-def scan_report():
+# Generate scan results report in XML
+def scan_report(target_url):
     """
     Downloads the scan report with current Scanner issues for
     URLs matching the specified urlPrefix (HTML/XML)
@@ -148,73 +196,41 @@ def scan_report():
         print("Error downloading the scan report: {}".format(e))
     else:
         print("[+] Downloading XML report for {}".format(target_url))
+
+        file_path = "/opt/stackstorm/scan_results/burp/burp_scan.xml"
+
         # Write the response body (byte array) to file
-        file_name = "burp-report_{}_{}.xml".format(
-            time.strftime("%Y%m%d-%H%M%S", time.localtime()),
-            target_url.replace("://", "-"),
-        )
-        file = os.path.join(tempfile.gettempdir(), file_name)
-        # remove 0x85 byte from raw response
-        data = r.text
-        data = re.sub(r'85', '', data)
-        data = re.sub(r'c1', '', data)
-
-        # write results to file
-        with open(file, 'wb') as f:
+        with open(file_path, "wb+") as f:
+            data = r.text
+            # remove 0x85 byte from raw response
+            data = re.sub(r'85', '', data)
+            data = re.sub(r'c1', '', data)
             f.write(data)
-        print("[+] Scan report saved to {}".format(file))
-        return file_name
 
-def main():
-    # adding target to scope
-    print("[+] Adding target url to scope ...")
-    add_scope()
+        print("[+] Scan report saved to {}".format(file_path))
 
-    """
-    Need better way to know when spider queue = 0.
-    Currently, just waiting a static amount of time.
-    """
-    print("[+] Spidering target url ...")
-    spider_target()
-    
-    waiting = 0
-    while waiting != 100:
-        if waiting % 5 == 0:
-            print("Still Spidering")
-        time.sleep(1)
-        waiting += 1
+def burp_stop():
+    """Stop the Burp Suite"""
+    # Because of an issue in burp-rest-api
+    # (https://github.com/vmware/burp-rest-api/issues/15),
+    # we can't Reset/Restore the Burp State, so we need to stop
+    # the Burp after the scan to reset the state.
+    # e.g. You can use a supervisord configuration to restart the
+    # Burp when it stopped running:
+    #   [program:burp-rest-api]
+    #   command=java -jar /opt/burp-rest-api/build/libs/burp-rest-api-1.0.0.jar
+    #   directory=/opt/burp-rest-api/build/libs
+    #   redirect_stderr=true
+    #   stdout_logfile=/var/log/burp-rest-api.log
+    #   autorestart=true
+    #   user=burpa
+    try:
+        r = requests.get(
+            "{}:{}/burp/stop".format(proxy_url, api_port)
+        )
+	r.raise_for_status()
+        print("[-] Burp is stopped")
+    except requests.exceptions.RequestException as e:
+        print("Error stopping the burp: {}".format(e))
 
-    print("[+] Spidering complete ...")
-
-    # check if site map content exists
-    print("[+] Checking spidered URLs ...")
-    content = site_map()
-    if not content:
-        print("No URL's have been discovered, scan cannot proceed.")
-        sys.exit(1)
-    else:
-        print("URL's discovered:")
-        for url in content['messages']:
-            print(url['url'])
-
-    print("[+] Performing active scan ...")
-    active_scan()
-
-    # get scan status
-    while scan_status() != 100:
-        time.sleep(20)
-    print("\n[+] Scan completed")
-
-    # get scan report
-    report = scan_report()
-
-if __name__ == '__main__':
-    # check usage
-    if len(sys.argv) != 2:
-        print("ERROR: Too few arguments.\n")
-        print("usage:   python burpauto.py [target_url]")
-        print("example: python burpauto.py http://example.com")
-    # set target URL and run main program
-    else:
-        target_url = sys.argv[1]
-        main()
+ 
